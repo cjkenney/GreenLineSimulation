b238241ba7c1fc12ab84abc261d469749dec91c2
CJ Kenney <kenne968@umn.edu>
2016-04-15 15:51:28 -0500

    Skeleton
    Skeleton files taken from Discrete Event lecture

---
+++ src/Event.java
@@ -0,0 +1,11 @@
+/**
+ * Created by cj on 4/10/16.
+ */
+// Priority Queue and Simulation
+// Event is the generic interface for a runnable agenda object.
+
+public interface Event {
+
+    void run();
+
+}
